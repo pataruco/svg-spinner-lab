253325115060f8b2eda110d094248b8b8f9ffec5
Pedro Martin <pedro.martin@red-badger.com>
2020-06-04 15:36:21 +0100

    Add dialog box

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
+// Animation trigger
 const successButton = document.querySelector("menu li:first-of-type button");
 const failureButton = document.querySelector("menu li:nth-of-type(2) button");
 const resetButton = document.querySelector("menu li:last-of-type button");
 const svg = document.querySelector("svg");
 
 const setSvgTo = (className) => {
-  console.log("hi");
+  // hideDialog();
   className
     ? svg.classList.toggle(className)
     : svg.classList.remove(...svg.classList);
@@ -13,3 +14,26 @@ const setSvgTo = (className) => {
 successButton.addEventListener("click", () => setSvgTo("success"));
 failureButton.addEventListener("click", () => setSvgTo("failure"));
 resetButton.addEventListener("click", () => setSvgTo(null));
+
+// Animation ended handlers
+
+const checkmark = document.querySelector("svg .checkmark polyline");
+const crossLastLine = document.querySelector("svg .cross line:last-of-type");
+const dialogBox = document.querySelector("dialog");
+
+const animatedElements = [checkmark, crossLastLine];
+
+const showDialog = (_event) => {
+  dialogBox.setAttribute("open", true);
+};
+
+const hideDialog = (_event) => {
+  // TODO: Remove atribute
+  [...dialogBox.attributes].forEach((attr) =>
+    dialogBox.removeAttribute(attr.name)
+  );
+};
+
+animatedElements.forEach((element) =>
+  element.addEventListener("animationend", showDialog)
+);
